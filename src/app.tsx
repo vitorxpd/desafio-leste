d9df712936ad915bf5463118be16e8e96da22780
Vitor Diniz <dinizadroit@gmail.com>
2024-06-28 17:07:30 -0300

    chore: setup

---
+++ src/app.tsx
@@ -0,0 +1,14 @@
+export function App() {
+  return (
+    <h1
+      style={{
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+      }}
+    >
+      Hello World
+    </h1>
+  )
+}
