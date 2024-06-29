9a607bb42387171ac8cd1d0521e90e9c72dc21cf
Vitor Diniz <dinizadroit@gmail.com>
2024-06-29 12:40:51 -0300

    feat: leste logo improvements

--- src/views/components/header.tsx
+++ src/views/components/header.tsx
@@ -4,7 +4,7 @@ export function Header() {
   return (
     <header className="mb-8 pt-8">
       <section className="mx-auto flex items-center justify-center px-8">
-        <img src={logo} alt="Logo" className="max-h-[260px] max-w-[180px]" />
+        <img src={logo} alt="Logo" className="w-[180px]" />
       </section>
     </header>
   )
