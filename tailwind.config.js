2a02790f28fe53f99e81f14b359d69c5b253a1e5
Vitor Diniz <dinizadroit@gmail.com>
2024-06-29 02:31:54 -0300

    fix: spinner animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -70,10 +70,15 @@ module.exports = {
           from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: '0' },
         },
+        rotation: {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        rotation: 'rotation 1s linear infinite',
       },
     },
   },
