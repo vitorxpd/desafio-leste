28eb37e16c6f8132e57f3633b83f1ef37d163061
Vitor Diniz <dinizadroit@gmail.com>
2024-06-29 17:50:50 -0300

    feat: add link to home on header logo

--- src/views/components/header.tsx
+++ src/views/components/header.tsx
@@ -1,10 +1,14 @@
+import { Link } from 'react-router-dom'
+
 import logo from '@/assets/logo.png'
 
 export function Header() {
   return (
     <header className="mb-8 pt-8">
       <section className="mx-auto flex items-center justify-center px-8">
-        <img src={logo} alt="Logo" className="w-[180px]" />
+        <Link to="/">
+          <img src={logo} alt="Logo" className="w-[180px]" />
+        </Link>
       </section>
     </header>
   )
