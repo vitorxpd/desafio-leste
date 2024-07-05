8dd0df699adf40e2d074ce75c4960b4247da9caf
Vitor Diniz <dinizadroit@gmail.com>
2024-07-05 00:08:00 -0300

    feat: create statistics drawer

---
+++ src/pages/home/components/statistics-content.tsx
@@ -0,0 +1,127 @@
+import { useMemo } from 'react'
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { useContacts } from '@/contexts/contacts-context'
+
+export function StatisticsContent() {
+  const { contacts } = useContacts()
+
+  const genderStatistics = useMemo(() => {
+    const maleCount = contacts.filter(
+      (contact) => contact.gender === 'M',
+    ).length
+    const femaleCount = contacts.filter(
+      (contact) => contact.gender === 'F',
+    ).length
+    const totalCount = contacts.length
+
+    const malePercentage = ((maleCount / totalCount) * 100).toFixed(2) + '%'
+    const femalePercentage = ((femaleCount / totalCount) * 100).toFixed(2) + '%'
+
+    return [
+      { gender: 'Male', count: maleCount, percentage: malePercentage },
+      { gender: 'Female', count: femaleCount, percentage: femalePercentage },
+    ]
+  }, [contacts])
+
+  const languageStatistics = useMemo(() => {
+    const totalContacts = contacts.length
+    const languageCount: {
+      language: string
+      count: number
+      percentage: number | string
+    }[] = []
+
+    contacts.forEach((contact) => {
+      const { language } = contact
+
+      const existingLanguage = languageCount.find(
+        (lang) => lang.language === language,
+      )
+
+      if (existingLanguage) {
+        existingLanguage.count++
+      } else {
+        languageCount.push({
+          language: language,
+          count: 1,
+          percentage: 1 / totalContacts,
+        })
+      }
+    })
+
+    languageCount.forEach((lang) => {
+      lang.percentage = ((lang.count / totalContacts) * 100).toFixed(2) + '%'
+    })
+
+    return languageCount
+  }, [contacts])
+
+  return (
+    <div className="z-50 mt-6 flex flex-col gap-4 p-4 md:flex-row md:overflow-hidden md:p-0">
+      <Card className="sm:w-1/2">
+        <CardHeader>
+          <CardTitle className="text-2xl font-semibold">
+            Gender Statistics
+          </CardTitle>
+
+          <CardDescription>Breakdown of users by gender.</CardDescription>
+        </CardHeader>
+        <CardContent className="flex flex-col gap-4">
+          {genderStatistics
+            .sort((a, b) => b.count - a.count)
+            .map((gender) => (
+              <div
+                key={gender.gender}
+                className="flex items-center justify-between"
+              >
+                <div>{gender.gender}</div>
+
+                <div className="font-medium">
+                  {gender.count}{' '}
+                  <span className="text-muted-foreground">
+                    ({gender.percentage})
+                  </span>
+                </div>
+              </div>
+            ))}
+        </CardContent>
+      </Card>
+
+      <Card className="flex flex-col md:w-1/2 md:overflow-hidden">
+        <CardHeader>
+          <CardTitle className="text-2xl font-semibold">
+            Language Statistics
+          </CardTitle>
+          <CardDescription>Top languages used on the site.</CardDescription>
+        </CardHeader>
+
+        <CardContent className="flex flex-col gap-4 no-scrollbar md:flex-1 md:overflow-scroll">
+          {languageStatistics
+            .sort((a, b) => b.count - a.count)
+            .map((language) => (
+              <div
+                key={language.language}
+                className="flex items-center justify-between"
+              >
+                <div>{language.language}</div>
+
+                <div className="font-medium">
+                  {language.count}{' '}
+                  <span className="text-muted-foreground">
+                    ({language.percentage})
+                  </span>
+                </div>
+              </div>
+            ))}
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
--- src/pages/home/components/statistics-modal.tsx
+++ src/pages/home/components/statistics-modal.tsx
@@ -1,12 +1,3 @@
-import { useMemo } from 'react'
-
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card'
 import {
   Dialog,
   DialogContent,
@@ -14,7 +5,16 @@ import {
   DialogHeader,
   DialogTitle,
 } from '@/components/ui/dialog'
-import { useContacts } from '@/contexts/contacts-context'
+import {
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerHeader,
+  DrawerTitle,
+} from '@/components/ui/drawer'
+import { isMobile } from '@/lib/utils'
+
+import { StatisticsContent } from './statistics-content'
 
 interface IStatisticsModalProps {
   open: boolean
@@ -22,125 +22,39 @@ interface IStatisticsModalProps {
 }
 
 export function StatisticsModal({ open, onClose }: IStatisticsModalProps) {
-  const { contacts } = useContacts()
-
-  const genderStatistics = useMemo(() => {
-    const maleCount = contacts.filter(
-      (contact) => contact.gender === 'M',
-    ).length
-    const femaleCount = contacts.filter(
-      (contact) => contact.gender === 'F',
-    ).length
-    const totalCount = contacts.length
-
-    const malePercentage = ((maleCount / totalCount) * 100).toFixed(2) + '%'
-    const femalePercentage = ((femaleCount / totalCount) * 100).toFixed(2) + '%'
-
-    return [
-      { gender: 'Male', count: maleCount, percentage: malePercentage },
-      { gender: 'Female', count: femaleCount, percentage: femalePercentage },
-    ]
-  }, [contacts])
-
-  const languageStatistics = useMemo(() => {
-    const totalContacts = contacts.length
-    const languageCount: {
-      language: string
-      count: number
-      percentage: number | string
-    }[] = []
-
-    contacts.forEach((contact) => {
-      const { language } = contact
-
-      const existingLanguage = languageCount.find(
-        (lang) => lang.language === language,
-      )
-
-      if (existingLanguage) {
-        existingLanguage.count++
-      } else {
-        languageCount.push({
-          language: language,
-          count: 1,
-          percentage: 1 / totalContacts,
-        })
-      }
-    })
-
-    languageCount.forEach((lang) => {
-      lang.percentage = ((lang.count / totalContacts) * 100).toFixed(2) + '%'
-    })
-
-    return languageCount
-  }, [contacts])
-
   return (
-    <Dialog open={open} onOpenChange={onClose}>
-      <DialogContent className="flex h-full w-full max-w-full flex-col overflow-scroll pt-10 sm:h-[420px] sm:max-w-[640px] sm:flex-row sm:overflow-hidden">
-        <DialogHeader className="hidden">
-          <DialogTitle>Statiscs</DialogTitle>
-          <DialogDescription>Gender and Language</DialogDescription>
-        </DialogHeader>
+    <>
+      {isMobile && (
+        <Drawer open={open} onClose={onClose}>
+          <DrawerContent
+            className="max-h-[320px] overflow-auto no-scrollbar"
+            onInteractOutside={onClose}
+          >
+            <DrawerHeader className="hidden">
+              <DrawerTitle>Statiscs</DrawerTitle>
+              <DrawerDescription>Gender and Language</DrawerDescription>
+            </DrawerHeader>
 
-        <Card className="sm:w-1/2">
-          <CardHeader>
-            <CardTitle className="text-2xl font-semibold">
-              Gender Statistics
-            </CardTitle>
+            <StatisticsContent />
+          </DrawerContent>
+        </Drawer>
+      )}
 
-            <CardDescription>Breakdown of users by gender.</CardDescription>
-          </CardHeader>
-          <CardContent className="flex flex-col gap-4">
-            {genderStatistics
-              .sort((a, b) => b.count - a.count)
-              .map((gender) => (
-                <div
-                  key={gender.gender}
-                  className="flex items-center justify-between"
-                >
-                  <div>{gender.gender}</div>
+      {!isMobile && (
+        <Dialog open={open} onOpenChange={onClose}>
+          <DialogContent
+            className="h-[420px] max-w-[640px]"
+            onInteractOutside={onClose}
+          >
+            <DialogHeader className="hidden">
+              <DialogTitle>Statiscs</DialogTitle>
+              <DialogDescription>Gender and Language</DialogDescription>
+            </DialogHeader>
 
-                  <div className="font-medium">
-                    {gender.count}{' '}
-                    <span className="text-muted-foreground">
-                      ({gender.percentage})
-                    </span>
-                  </div>
-                </div>
-              ))}
-          </CardContent>
-        </Card>
-
-        <Card className="flex flex-col overflow-hidden sm:w-1/2">
-          <CardHeader>
-            <CardTitle className="text-2xl font-semibold">
-              Language Statistics
-            </CardTitle>
-            <CardDescription>Top languages used on the site.</CardDescription>
-          </CardHeader>
-
-          <CardContent className="flex flex-1 flex-col gap-4 overflow-scroll no-scrollbar">
-            {languageStatistics
-              .sort((a, b) => b.count - a.count)
-              .map((language) => (
-                <div
-                  key={language.language}
-                  className="flex items-center justify-between"
-                >
-                  <div>{language.language}</div>
-
-                  <div className="font-medium">
-                    {language.count}{' '}
-                    <span className="text-muted-foreground">
-                      ({language.percentage})
-                    </span>
-                  </div>
-                </div>
-              ))}
-          </CardContent>
-        </Card>
-      </DialogContent>
-    </Dialog>
+            <StatisticsContent />
+          </DialogContent>
+        </Dialog>
+      )}
+    </>
   )
 }
